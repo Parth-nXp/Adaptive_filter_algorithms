b96b7d5735ad5a85800327ea15598c92d8177507
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2022-10-18 15:13:23 +0530

    Update TLMM.m

--- TLMM.m
+++ TLMM.m
@@ -6,13 +6,13 @@ clear all;
 channel_taps = 16; % number of channel taps present in the FIR filter
 desired_noise_SNR = 0; % gaussian noise present in the desired output data
 filter_weights = rand(channel_taps,1); % initializing normalized random values for the channel taps of FIR filter
-weight_update = zeros(channel_taps,1); % inital guess of the filter weights choosen to be all zero vector
+weight_update = rand(channel_taps,1); % inital guess of the filter weights choosen to be all zero vector
 step_size = 0.01; % step size or step length
 
 
 wait_bar = waitbar(0,'Starting processing');
-mu_LMS = step_size; % step length of the MCC update method
-experiment= 1000; % ensemble-average independent runs
+mu_TLMM = step_size; % step length of the MCC update method
+experiment= 10; % ensemble-average independent runs
 iteration = 5000; % total number of iterations done
 
 
@@ -30,7 +30,7 @@ for dummy_var_2 = 1:experiment
     mean_square_deviation = zeros(iteration,1); % Mean Square Deviation
     mean_square_error = zeros(iteration,1); % Mean Square Error
     excess_mean_square_error = zeros(iteration,1); % Excess Mean Square Error
-    w_LMS = weight_update; % setting the weight update vector equal to the initial guess which is all zero vector
+    w_TLMM = weight_update; % setting the weight update vector equal to the initial guess which is all zero vector
 
 
     for dummy_var = 1:iteration
@@ -39,11 +39,11 @@ for dummy_var_2 = 1:experiment
         u_i = [new_tx_symbol u_i(1:end-1)]; % generate regressor/input signal (u_i - a row vector of size 1xM)
         [d_i,desired_noise_variance] = awgn(u_i*filter_weights, desired_noise_SNR); % generate noisy version of channel output as received symbol
         e_i_TLMM = (d_i -u_i*w_TLMM); % finding error between desired output and filter output to update adaptive filter
-        w_TLMM = w_TLMM + mu_TLMM*((norm(w_TLMM)^2*e_i_TLMM*u_i'+e_i_TLMM^2*w_TLMM)/norm(w_TLMM)^4); % updating the adaptive filter after finding the error using LMS algorithm
+        w_TLMM = w_TLMM + mu_TLMM*((norm(w_TLMM)^2*e_i_TLMM*u_i'+e_i_TLMM^2*w_TLMM)/norm(w_TLMM)^4); % updating the adaptive filter after finding the error using TLMM algorithm
         
         %calculation of the parameter
-        mean_square_deviation(dummy_var) = norm(w_LMS-filter_weights)^2; % mean square deviation calculation
-        excess_mean_square_error(dummy_var) = norm(e_i_LMS)^2; % Excess Mean Square Error calculation
+        mean_square_deviation(dummy_var) = norm(w_TLMM-filter_weights)^2; % mean square deviation calculation
+        excess_mean_square_error(dummy_var) = norm(e_i_TLMM)^2; % Excess Mean Square Error calculation
         mean_square_error(dummy_var) = excess_mean_square_error(dummy_var)+ desired_noise_variance; % Mean Square Error Calculation
     end
 
